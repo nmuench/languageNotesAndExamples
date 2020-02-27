6435dfd3d3f65d48d8128b7cf47c988842312878
nmuench <nmuench@clemson.edu>
2020-02-27 14:54:17 -0500

    Added Python IO

---
+++ InputAndOutput/CommandLineInput/Python/commandLineInput.py
@@ -0,0 +1,3 @@
+import sys
+print(f"{sys.argv[1]} is the first command line argument")
+print("There are {} total command line arguments, including the filename".format(len(sys.argv)))
---
+++ InputAndOutput/TerminalInputAndOutput/Python/terminalIO.py
@@ -0,0 +1,3 @@
+print("This prints to the terminal")
+inString = input("Type something in: ")
+print(f"{inString} was given as input")
